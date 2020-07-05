474ce85becfcd488a0a4f00f0852963774185c91
Perry Brandiezs <pbrandiezs@gmail.com>
2020-07-04 21:49:33 -0500

    feat: Add a timestamp array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   clicked = false;
+  buttonClicks = [];
+
 
   onclick(){
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date+' '+ time;
+
     if (!this.clicked) {
       this.clicked = true;
+      this.buttonClicks.push(dateTime);
     } else {
       this.clicked = false;
+      this.buttonClicks.push(dateTime);
     }
+    console.log(this.buttonClicks);
   }
 }
